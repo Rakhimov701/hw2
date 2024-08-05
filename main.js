0db275dcfe977a4559df66306a2c6885aed0893c
Rakhimov <urakhimov8817@gmail.com>
2024-08-05 19:22:22 +0500

    hw1

---
+++ main.js
@@ -0,0 +1,22 @@
+var myName = prompt("Введите ваше имя:");
+console.log("Ваше имя  " + myName );
+
+var age = prompt("Введите ваш возраст:");
+console.log("Ваше возраст  " + age );
+
+var q1 = +prompt("Решите пример: 7 + 4");
+console.log("7 + 4 = 11 (" + myName + " Ваш ответ " + q1 + ")");
+
+var q2 = +prompt("Решите пример: 11 - 4");
+console.log("11 - 4 = 7 (" + myName + " Ваш ответ " + q2 + ")");
+
+var q3 = +prompt("Решите пример: 7 * 3");
+console.log("7 * 3 = 21 (" + myName + " Ваш ответ " + q3 + ")");
+
+var q4 = +prompt("Решите пример: 8 / 2");
+console.log("8 / 2 = 4 (" + myName + " Ваш ответ " + q4 + ")");
+
+var q5 = +prompt("Решите пример: 10 % 3");
+console.log("10 % 3 = 1 (" + myName + " Ваш ответ " + q5 + ")");
+
+alert("Проверьте консоль для результатов.");
